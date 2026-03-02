cadfe3fb35429575d751954c613c78c7dfe56685
poonamrathia05-stack <poonam.rathia05@gmail.com>
2026-03-02 13:19:55 +0530

    Add files via upload

---
+++ DivisibleBy5Not3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class DivisibleBy5Not3 {
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter range limit: ");
+      int n = sc.nextInt();
+
+      for(int i = 1; i <= n; i++) {
+         if(i % 5 == 0 && i % 3 != 0)
+            System.out.print(i + " ");
+      }
+   }
+}
